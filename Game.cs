37df994c48c3ad737a968342977f442c24a6b803
l <l>
2021-06-24 20:05:25 +0500

    finished, but without any style

--- Game.cs
+++ Game.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace ChessWithClasses
 {
-	class Game
+	public class Game
 	{
+		private readonly IDrawer drawer;
+		private Menu menu;
+
+		public Game(IDrawer drawer)
+		{
+			this.drawer = drawer;
+			menu = new Menu(drawer);
+		}
+
+		public void Run()
+		{
+			menu.Draw();
+		}
 	}
 }
--- Player.cs
+++ Player.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChessWithClasses
 {
-	class Player
+	public class Player
 	{
 	}
 }
